45623d15a7259602bae63ebb3f9e08a087ce0c29
ericsmith92 <ericsmth884@gmail.com>
2020-06-27 16:02:06 -0400

    update table with html bare bones

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -1,11 +1,12 @@
 window.onload = () => {
     const btnSelect = document.querySelector('.queryDb');
     const btnInsert = document.querySelector('.insertDb');
+    const root = document.querySelector('.root');
 
     const queryDb = () => {
         fetch('/query')
             .then(res => res.json())
-            .then(json => console.log(json))
+            .then(json => buildTable(json))
             .catch(err => console.log(err));
     }
 
@@ -25,8 +26,33 @@ window.onload = () => {
         
         return response.json();
     }
-    
 
+    const buildTable = ({ data }) => {
+        const table = document.createElement('table');
+        const tableHead = buildTableHead(data[0]);
+        const tableBody = buildTableBody(data);
+
+        console.log(tableBody);
+
+        table.insertAdjacentHTML('afterbegin', tableHead);
+        table.insertAdjacentHTML('beforeend', tableBody);
+       
+        
+        root.appendChild(table);
+    };
+
+    const buildTableHead = (obj) => {
+        const headings = Object.keys(obj);
+        const headingsThs = headings.map(heading => `<th>${heading}</th>`);
+        
+        return ['<tr>', ...headingsThs , '</tr>'].join(''); 
+    }
+
+    const buildTableBody = (arr) => {
+        const bodyTds = arr.map(row => `<tr><td>${row.name}</td><td>${row.price}</td><td>${row.id}</td></tr>`);
+        return bodyTds.join(''); 
+    }
+    
     btnSelect.addEventListener('click', queryDb);
     btnInsert.addEventListener('click', insertDb);
 }
